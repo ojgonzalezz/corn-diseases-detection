e467b13029a2467b28c76f083856c1dbc1ff8cf0
afelipfo <afelipeflorezo@gmail.com>
2025-10-08 19:04:36 -0500

    Limpiar rama pipe: eliminar archivos del proyecto anterior

--- experiments/edge_models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Experimentos de modelos edge para dispositivos de borde.
-
-Este paquete contiene scripts para entrenar, comparar y seleccionar
-arquitecturas livianas optimizadas para edge computing.
-"""
-
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Tests para el proyecto de Detección de Enfermedades del Maíz.
-"""
